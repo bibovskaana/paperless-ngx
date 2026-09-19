94629753caa32a632fa749bf9941aeef6e6965ad
Ana Bibovska <ana.bibovska@students.finki.ukim.mk>
2026-09-19 21:31:30 +0200

    Update Docker configuration

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "Running database migrations..."
+python3 manage.py migrate --noinput
+
+echo "Starting Granian..."
+exec granian --interface wsgi paperless.wsgi:application --host 0.0.0.0 --port 8000
